1d1122d17bf6e23afc99ccdcc62283a1cec17a60
nitrolanco <mamibalss@gmail.com>
2023-04-17 14:57:24 +0200

    Update t1ia.py

--- t1ia.py
+++ t1ia.py
@@ -25,9 +25,21 @@ def dfsNext(current, arcos):
     possibles = []
     for a in arcos:
         if a[0] == current:
-            print(current)
             possibles.append(a)
     return possibles[random.randint(0, len(possibles)-1)]
+
+
+def pathPrinter(path):
+    for i in range(0, len(path)):
+        if i < len(path)-1:
+            print(path[i] + "->", end="")
+        else:
+            print(path[i])
+
+
+def onceExpanded(path):
+    for i in range(0, len(path)):
+        print(path[i] + ": 1", end="\n")
 
 
 def dfs(initNode, finalNode, arcos):
@@ -39,6 +51,10 @@ def dfs(initNode, finalNode, arcos):
         next = dfsNext(current, arcos)
         totalCost += next[2]
         current = next[1]
+        visited.append(current)
+    pathPrinter(visited)
+    print("Costo: "+str(totalCost))
+    onceExpanded(visited)
     return totalCost
 
 
@@ -48,9 +64,7 @@ def greedyNext(current, valHeuristica, arcos):
         if a[0] == current:
             print(current)
             val = 0
-            for b in valHeuristica:
-                if b[0] == a[1]:
-                    val = b[1]
+            getVal(a[1], valHeuristica)
             possibles.append((a[2]+val, a))
     sortedPossibles = sorted(possibles)
     return sortedPossibles[0][1]
@@ -59,10 +73,16 @@ def greedyNext(current, valHeuristica, arcos):
 def greedy(initNode, finalNode, valHeuristica, arcos):
     totalCost = 0
     current = initNode
+    visited = []
+    visited.append(current)
     while (current != finalNode):
         next = greedyNext(current, valHeuristica, arcos)
         totalCost += next[2]
         current = next[1]
+        visited.append(current)
+    pathPrinter(visited)
+    print("Costo: "+str(totalCost))
+    onceExpanded(visited)
     return totalCost
 
 
@@ -112,4 +132,39 @@ def aStar(initNode, finalNode, valHeuristica, arcos):
     return currPath[0]
 
 
-print(aStar(initNode, finalNode, valHeuristica, arcos))
+def avUC(currPath, arcos):
+    avaibles = []
+    for a in arcos:
+        if a[0] == currPath[-1]:
+            posNext = []
+            posNext = currPath[:]
+            posNext.append(a[1])
+            posNext[0] = a[2]+posNext[0]
+            avaibles.append(posNext)
+    return avaibles
+
+
+def uCost(initNode, finalNode, arcos):
+    pQ = []
+    visited = []
+    current = initNode
+    currPath = [0, current]
+    pQ.append(currPath)
+    while (current != finalNode):
+        avaibles = []
+        avaibles = avUC(currPath, arcos)
+        for a in avaibles:
+            pQ.append(a)
+        visited.append(pQ[0])
+        pQ.pop(0)
+        pQ = sorted(pQ)
+        current = pQ[0][-1]
+        currPath = pQ[0]
+    visited.append(pQ[0])
+    for a in visited:
+        print(a)
+    return currPath[0]
+
+
+print(greedy(initNode, finalNode, valHeuristica, arcos))
+# print(dfs(initNode, finalNode, arcos))
